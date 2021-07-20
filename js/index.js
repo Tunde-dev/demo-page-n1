f0d6a8ed02794f1444885a83cc49a595b1bc36f3
Veres Tünde <tunderke86@gmail.com>
2021-07-20 10:56:06 +0200

    creating a mobile version of the nav menu

--- js/index.js
+++ js/index.js
@@ -1,12 +1,34 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {stickyNav()};
 
-let navbar = document.querySelector(".header-content");
-var sticky = navbar.offsetTop;
+let navbar = document.querySelector(".main-nav");
+let sticky = navbar.offsetTop;
 
-function myFunction() {
+function stickyNav() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
   }
+}
+
+const hamburger = document.querySelector(".hamburger");
+const offCanvas = document.querySelector(".offcanvas");
+const navItem = document.querySelector(".nav-item");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    //hamburger.classList.toggle("show");
+    offCanvas.classList.toggle("show");
+    //navItem.classList.toggle("active");
+}
+
+const closeNav = document.querySelectorAll(".close-nav");
+
+closeNav.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    //hamburger.classList.remove("active");
+    offCanvas.classList.remove("show");
+    //navItem.classList.remove("active");
 }
