f5a8c18caa53e481eca4e046313a240a5b0f2c54
Veres Tünde <tunderke86@gmail.com>
2021-07-20 20:57:44 +0200

    add slideshow to the fourth section

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
-window.onscroll = function() {stickyNav()};
+window.onscroll = function () {
+  stickyNav();
+};
 
 let navbar = document.querySelector(".main-nav");
 let sticky = navbar.offsetTop;
 
 function stickyNav() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
   }
@@ -18,17 +20,70 @@ const navItem = document.querySelector(".nav-item");
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-    //hamburger.classList.toggle("show");
-    offCanvas.classList.toggle("show");
-    //navItem.classList.toggle("active");
+  offCanvas.classList.toggle("show");
 }
 
 const closeNav = document.querySelectorAll(".close-nav");
 
-closeNav.forEach(n => n.addEventListener("click", closeMenu));
+closeNav.forEach((n) => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
-    //hamburger.classList.remove("active");
-    offCanvas.classList.remove("show");
-    //navItem.classList.remove("active");
+  offCanvas.classList.remove("show");
+}
+
+let slideIndex = 1;
+let myTimer;
+let slideshowContainer;
+
+const pause = () => {
+  clearInterval(myTimer);
+}
+const resume = () => {
+  clearInterval(myTimer);
+  myTimer = setInterval(function(){plusSlides(slideIndex)}, 3000)
+}
+
+window.addEventListener("load", () => {
+  showSlides(slideIndex);
+  myTimer = setInterval(function(){plusSlides(1)}, 3000);
+  slideshowContainer = document.getElementsByClassName("slideshow-container")[0];
+
+  slideshowContainer.addEventListener("mouseenter", pause);
+  slideshowContainer.addEventListener("mouseleave", resume)
+})
+
+const plusSlides = (n) => {
+  clearInterval(myTimer);
+  if (n < 0) {
+    showSlides(slideIndex -= 1)
+  } else {
+    showSlides(slideIndex += 1)
+  }
+  if (n === -1) {
+    myTimer = setInterval(function(){plusSlides(n + 2)}, 3000)
+  } else {
+    myTimer = setInterval(function(){plusSlides(n + 1)}, 3000)
+  }
+}
+
+function currentSlide(n) {
+  clearInterval(myTimer);
+  myTimer = setInterval(function(){plusSlides(n + 1)}, 3000)
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("testimonial-wrapper");
+  const dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
