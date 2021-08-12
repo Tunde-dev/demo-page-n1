306335578cf1b69aa3b09b190a65453fd79db03c
Veres Tünde <tunderke86@gmail.com>
2021-08-12 15:48:57 +0200

    slideshow refactoring and added slideshow to slide-menu

--- js/index.js
+++ js/index.js
@@ -31,60 +31,8 @@ function closeMenu() {
   offCanvas.classList.remove("show");
 }
 
-let slideIndex = 1;
-let myTimer;
-let slideshowContainer;
-
-const pause = () => {
-  clearInterval(myTimer);
-}
-const resume = () => {
-  clearInterval(myTimer);
-  myTimer = setInterval(function(){plusSlides(slideIndex)}, 3000)
-}
-
 window.addEventListener("load", () => {
-  showSlides(slideIndex);
-  myTimer = setInterval(function(){plusSlides(1)}, 3000);
-  slideshowContainer = document.getElementsByClassName("slideshow-container")[0];
-
-  slideshowContainer.addEventListener("mouseenter", pause);
-  slideshowContainer.addEventListener("mouseleave", resume)
-})
-
-function plusSlides(n) {
-  clearInterval(myTimer);
-  if (n < 0) {
-    showSlides(slideIndex -= 1)
-  } else {
-    showSlides(slideIndex += 1)
-  }
-  if (n === -1) {
-    myTimer = setInterval(function(){plusSlides(n + 2)}, 3000)
-  } else {
-    myTimer = setInterval(function(){plusSlides(n + 1)}, 3000)
-  }
-}
-
-function currentSlide(n) {
-  clearInterval(myTimer);
-  myTimer = setInterval(function(){plusSlides(n + 1)}, 3000)
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  const slides = document.getElementsByClassName("testimonial-wrapper");
-  const dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+  const slider1 = new Slider(document.getElementById('slider-1'));
+  const slider2 = new Slider(document.getElementById("slider-2"));
+});
 
---
+++ js/slider.js
@@ -0,0 +1,124 @@
+class Slider {
+  constructor(sliderElem) {
+    this.slider = sliderElem;
+    this.sliderItems = sliderElem.getElementsByClassName("slider-item");
+    this.nextBtn = sliderElem.querySelector(".slider-control-next");
+    this.prevBtn = sliderElem.querySelector(".slider-control-prev");
+    this.indicators = sliderElem.getElementsByClassName("slide-indicator");
+    this.currentIndex = 0;
+    this.prevItemIndex = this.sliderItems.length - 1;
+    this.nextItemIndex = 1;
+    this.isSliding = false;
+    this.autoStart();
+    this.setEventListeners();
+    this.indicators[this.currentIndex].classList.add("active");
+
+    
+
+    
+  }
+
+  autoStart() {
+    this.autoStartInterval = setInterval(() => {
+      this.next();
+    }, 5000);
+  }
+
+  setEventListeners() {
+    this.prevBtn.addEventListener("click", () => {
+      this.prev();
+    });
+    this.nextBtn.addEventListener("click", () => {
+      this.next();
+    });
+    this.slider.addEventListener('mouseover', () => {
+      clearInterval(this.autoStartInterval);
+    });
+    this.slider.addEventListener('mouseout', () => {
+      this.autoStart();
+    });
+  }
+
+  next() {
+    console.log("next");
+    if (this.isSliding) return;
+    this.isSliding = true;
+    this.sliderItems[this.nextItemIndex].classList.add("next-item");
+    setTimeout(() => {
+      this.sliderItems[this.currentIndex].classList.add("slide-next");
+      this.sliderItems[this.nextItemIndex].classList.add("slide-end");
+      this.sliderItems[this.nextItemIndex].classList.add("active");
+    }, 20);
+    setTimeout(() => {
+      this.sliderItems[this.nextItemIndex].classList.remove(
+        "next-item",
+        "slide-end"
+      );
+      this.sliderItems[this.currentIndex].classList.remove(
+        "slide-next",
+        "active"
+      );
+      this.isSliding = false;
+      this.indicators[this.currentIndex].classList.remove("active");
+      this.indicators[this.nextItemIndex].classList.add("active");
+      this.setIndices("NEXT");
+    }, 400);
+  }
+
+  prev() {
+    console.log("prev");
+    if (this.isSliding) return;
+    this.isSliding = true;
+    this.sliderItems[this.prevItemIndex].classList.add("prev-item");
+    setTimeout(() => {
+      this.sliderItems[this.currentIndex].classList.add("slide-prev");
+      this.sliderItems[this.prevItemIndex].classList.add("slide-end");
+      this.sliderItems[this.prevItemIndex].classList.add("active");
+    }, 20);
+    setTimeout(() => {
+      this.sliderItems[this.prevItemIndex].classList.remove(
+        "prev-item",
+        "slide-end"
+      );
+      this.sliderItems[this.currentIndex].classList.remove(
+        "slide-prev",
+        "active"
+      );
+      this.isSliding = false;
+      this.indicators[this.currentIndex].classList.remove("active");
+      this.indicators[this.prevItemIndex].classList.add("active");
+      this.setIndices("PREV");
+    }, 400);
+  }
+
+  setIndices(direction) {
+    let index;
+    if (direction === "NEXT") {
+      index =
+        this.currentIndex === this.sliderItems.length - 1
+          ? 0
+          : this.currentIndex + 1;
+    }
+    if (direction === "PREV") {
+      index =
+        this.currentIndex === 0
+          ? this.sliderItems.length - 1
+          : this.currentIndex - 1;
+    }
+    if (index === 0) {
+      this.currentIndex = index;
+      this.nextItemIndex = index + 1;
+      this.prevItemIndex = this.sliderItems.length - 1;
+    } else if (index === this.sliderItems.length - 1) {
+      this.currentIndex = this.sliderItems.length - 1;
+      this.nextItemIndex = 0;
+      this.prevItemIndex = this.currentIndex - 1;
+    } else {
+      this.currentIndex = index;
+      this.nextItemIndex = index + 1;
+      this.prevItemIndex = index - 1;
+    }
+  }
+}
+/*const slider1 = new Slider(document.querySelector("#slider-1"));
+const slider2 = new Slider(document.querySelector("#slider-2"));*/
